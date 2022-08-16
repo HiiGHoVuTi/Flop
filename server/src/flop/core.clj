850290d4d13ab2b218f355304788b2ab26b03a6e
Maxime <emixampons@gmail.com>
2022-08-16 18:44:57 +0200

    ♻️: made use of the request body instead of relying on URLs

--- server/src/flop/core.clj
+++ server/src/flop/core.clj
@@ -1,8 +1,8 @@
 (ns flop.core
-  (:require [flop.env :as env]
+  (:require [flop.env :as env] [flop.util :as util]
             [org.httpkit.server :as server]
             [clojure.data.json :as json]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
             [compojure.coercions :refer [as-int]]
             [ring.util.response :refer [not-found]]
@@ -10,24 +10,28 @@
             [flop.db.init] [flop.db.song])
   (:gen-class))
 
+    
 (defroutes app-routes
   (GET "/" [] "hello world !")
   (GET "/song-info/:id" [id :<< as-int]
     (if-let [data (flop.db.song/to-json id)]
       (json/write-str data)
       (not-found "No song with this id")))
-  (GET "/song-search/:fuzzy" [fuzzy]
-    (if-let [data (flop.db.song/find-song-json fuzzy)]
+  (GET "/song-search/" req
+    (if-let [data (flop.db.song/find-song-json (util/body-as-text req))]
       (json/write-str data)
       (not-found "How did you query something so wrong ?")))
-  (GET "/artist/:name" [name]
-    (if-let [data (flop.db.song/artist-as-json name)]
+  (GET "/artist/" req
+    (if-let [data (flop.db.song/artist-as-json (util/body-as-text req))]
       (json/write-str data)
       (not-found "No artist with this exact name")))
-  (GET "/album/:name" [name]
-    (if-let [data (flop.db.song/album-as-json name)]
+  (GET "/album/" req
+    (if-let [data (flop.db.song/album-as-json (util/body-as-text req))]
       (json/write-str data)
       (not-found "No album with this exact name")))
+  (POST "/import-song/" req 
+    (let [query (util/body-as-text req)]
+      (flop.db.song/import-external! query)))
   (route/not-found "Error, page not found!"))
 
 (defn -main
